6b738a137029e6795060993ff9825bd5e5f7eca3
Tamil Arasan <74599391+tamilts124@users.noreply.github.com>
2026-03-22 16:31:44 +0000

    Delete Tools/Youtube-Downloader-Install.sh

--- Tools/Youtube-Downloader-Install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/tamilts124/Youtube-Downloader-Personnel.git
-
-cd Youtube-Downloader-Personnel
-
-docker build -t youtube-downloader .
-
-docker run -d -p 8000:8000 --name youtube-downloader-container youtube-downloader
